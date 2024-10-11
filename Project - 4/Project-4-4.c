f3572c5271b69ac85fee25d9c4282518d96e3e7c
Khushal vaghasiya <khushalvaghasiya0@gmail.com>
2024-10-11 11:26:43 +0530

    Add files via upload

---
+++ Project - 4/Project-4-4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/*
+1 0 1 0 1
+  1 0 1 0
+  	1 0 1
+	  1 0
+	   	1 
+*/
+main()
+{
+	int i,j,s;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(s=1;s<i;s++)
+		{
+			printf("  ");
+		}
+		for(j=5;j>=i;j--)
+		{
+			printf("%d ",j%2);
+		}
+		printf("\n");
+	}
+}
